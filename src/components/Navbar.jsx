cb1ceb528dab2af46094be2b2f055af0c6bc3c44
Juan I. Solis <solid.dogtraining@gmail.com>
2024-02-08 18:01:19 +0100

    first navbar commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">Logo</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <h1>Adoptable</h1>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
