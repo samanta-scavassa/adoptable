35efcb87edb08f83e42e33bb6b3ca7cd3c532b0e
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-02-14 10:39:58 +0100

    remove sadDog picture from contactPage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
-import sadDog from "../assets/images/sad-dog.jpg";
 import "./ContactPage.css";
 import ErrorPage from "./ErrorPage";
 
