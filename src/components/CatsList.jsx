b8d63ace1c6e2d0240acff1e30f634144a65f9c6
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-02-14 21:15:10 +0100

    fix search bar for cats

--- src/components/CatsList.jsx
+++ src/components/CatsList.jsx
@@ -1,44 +1,51 @@
 import PetCard from "./PetCard";
 import "./PetsList.css";
 import SearchBar from "./SearchBar";
-import { useState, useEffect} from "react";
+import { useState } from "react";
 
-function CatsList({ pets, isLoading }) {
-  const [cats, setCats] = useState(
-    pets.filter((pet) => pet.category === "cat")
-  );
+function CatsList({ pets }) {
+  // something that changes --> input in the search bar
+  const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    if (isLoading === false) {
-      setCats(pets.filter((pet) => pet.category === "cat"));
+  // a helper function to get all the pets with category 'cat'
+  const checkCatCategory = (pet) => pet.category === "cat";
+
+  const filteredCats = pets.filter(checkCatCategory).filter((cat) => {
+    if (query) {
+      // if there's a query, do the following
+      return (
+        // check the cat name OR the breed
+        cat.name.toLowerCase().includes(query) ||
+        cat.breed.toLowerCase().includes(query)
+      );
+    } else {
+      // if there's no query, keep all cats
+      return true;
     }
-    
-  }, [isLoading]);
-
-  if (isLoading) {
-    return <h1>We are loading, please wait...</h1>;
-  }
+  });
 
   return (
-    <div className="PetsListSection">
-      <SearchBar cats={cats} onFilter={setCats} />
-      {cats.map((cat) => {
-        const kitten = {
-          id: cat.id,
-          name: cat.name,
-          breed: cat.breed,
-          age: cat.date_of_birth,
-          desexed: cat.desexed,
-          gender: cat.gender,
-          microchipNumber: cat.microchip_number,
-          vaccinated: cat.vaccinated,
-          wormed: cat.wormed,
-          description: cat.description,
-          image: cat.image_url,
-        };
-        return <PetCard key={kitten.id} pet={kitten} />;
-      })}
-    </div>
+    <>
+      <SearchBar onFilter={setQuery} />
+      <div className="PetsListSection">
+        {filteredCats.map((cat) => {
+          const kitten = {
+            id: cat.id,
+            name: cat.name,
+            breed: cat.breed,
+            age: cat.date_of_birth,
+            desexed: cat.desexed,
+            gender: cat.gender,
+            microchipNumber: cat.microchip_number,
+            vaccinated: cat.vaccinated,
+            wormed: cat.wormed,
+            description: cat.description,
+            image: cat.image_url,
+          };
+          return <PetCard key={kitten.id} pet={kitten} />;
+        })}
+      </div>
+    </>
   );
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 
-export default function SearchBar({cats, onFilter}) {
-  const [query, setQuery] = useState([]);
+/**
+ * SearchBar component showing an input
+ */
+export default function SearchBar({ onFilter }) {
+  // State for controlling whatever gets entered into the search bar
+  const [query, setQuery] = useState("");
 
+  // a helper function that updates the state AND
+  // calls the prop 'onFilter' which updates the parent component
   const handleSearch = (e) => {
     setQuery(e.target.value);
-
-    const filteredCats = cats.filter((cat) => {
-      return e.target.value && cat.name.toLowerCase().includes(e.target.value);
-    });
-
-    onFilter(filteredCats);
+    onFilter(e.target.value);
   };
 
   return (
-    <main>
+    <div>
       <h3 className="searchbar">Search</h3>
       <input
         type="text"
@@ -22,6 +23,6 @@ export default function SearchBar({cats, onFilter}) {
         value={query}
         onChange={handleSearch}
       />
-    </main>
+    </div>
   );
 }
